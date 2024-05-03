8ebb3f4413a31aeec341412122040c36b7aafc3d
p2730681 <p2730681@my365.dmu.ac.uk>
2024-05-03 12:46:43 +0100

    Added testing for payment class

---
+++ Testing4/tstPayment.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstPayment
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create
+            clsPayment AnPayment = new clsPayment();
+            //test to see that it exixts
+            Assert.IsNotNull(AnPayment);
+        }
+    }
+}
