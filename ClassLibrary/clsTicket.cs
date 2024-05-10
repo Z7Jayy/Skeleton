001cfc159024f0cc959e1544c7838265c3b7137f
P2755702 <P2755702@my365.dmu.ac.uk>
2024-05-10 12:36:34 +0100

    added code for the propertiesof the table for all data types

---
+++ ClassLibrary/clsTicket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsTicket
+    {
+        public int TicketID { get; set; }
+        public DateTime Date { get; set; }
+        public int Price { get; set; }
+        public string Venue { get; set; }
+        public string Artist { get; set; }
+        public bool IsSold { get; set; }
+        public string TicketType { get; set; }
+    }
+}
