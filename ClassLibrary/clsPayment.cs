a8e410ca0ef8456867ec87c1dc9be5a1e15bc924
p2730681 <p2730681@my365.dmu.ac.uk>
2024-05-15 11:36:47 +0100

    Created find methods

--- ClassLibrary/clsPayment.cs
+++ ClassLibrary/clsPayment.cs
@@ -1,16 +1,155 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
 
 namespace ClassLibrary
 {
     public class clsPayment
     {
+        //private data member for the payment id property
+        private Int32 mPaymentID;
 
-        public int paymentID { get; set; }
-        public string transactionID { get; set; }
-        public double amount { get; set; }
-        public DateTime paymentDate { get; set; }
-        public bool isPaymentSuccessful { get; set; }
-        public string paymentMethod { get; set; }
-        public int ticketId { get; set; }
+        //paymentID public property
+        public Int32 PaymentID
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mPaymentID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mPaymentID = value;
+            }
+        }
+
+            //private data member for the payment id property
+            private string mTransactionID;
+
+            //paymentID public property
+            public string TransactionID
+            {
+                get
+                {
+                    //this line of code sends data out of the property
+                    return mTransactionID;
+                }
+                set
+                {
+                   //this line of code allows data into the property
+                   mTransactionID = value;
+                }
+            }
+
+            // Private data member for Amount property
+            private double mAmount;
+
+            // Amount public property
+            public double Amount
+            {
+                get
+                {
+                     // This line of code sends data out of the property
+                     return mAmount;
+                }
+                set
+                {
+                     // This line of code allows data into the property
+                     mAmount = value;
+                }
+            }
+
+
+        // Private data member for PaymentDate property
+        private DateTime mPaymentDate;
+
+        // PaymentDate public property
+        public DateTime PaymentDate
+        {
+            get
+            {
+                // This line of code sends data out of the property
+                return mPaymentDate;
+            }
+            set
+            {
+                // This line of code allows data into the property
+                mPaymentDate = value;
+            }
+        }
+
+
+        // Private data member for IsPaymentSuccessful property
+        private bool mIsPaymentSuccessful;
+
+        // IsPaymentSuccessful public property
+        public bool IsPaymentSuccessful
+        {
+            get
+            {
+                // This line of code sends data out of the property
+                return mIsPaymentSuccessful;
+            }
+            set
+            {
+                // This line of code allows data into the property
+                mIsPaymentSuccessful = value;
+            }
+        }
+
+
+        // Private data member for PaymentMethod property
+        private string mPaymentMethod;
+
+        // PaymentMethod public property
+        public string PaymentMethod
+        {
+            get
+            {
+                // This line of code sends data out of the property
+                return mPaymentMethod;
+            }
+            set
+            {
+                // This line of code allows data into the property
+                mPaymentMethod = value;
+            }
+        }
+
+
+        // Private data member for TicketID property
+        private int mTicketID;
+
+        // TicketID public property
+        public int TicketID
+        {
+            get
+            {
+                // This line of code sends data out of the property
+                return mTicketID;
+            }
+            set
+            {
+                // This line of code allows data into the property
+                mTicketID = value;
+            }
+        }
+
+
+        public bool Find(int paymentID)
+        {
+            //set the private data members to the test data value
+            mPaymentID = 1;
+            mTransactionID = "ABC-123";
+            mAmount = 85.50;
+            mPaymentDate = Convert.ToDateTime("01/05/2024");
+            mIsPaymentSuccessful = true;
+            mPaymentMethod = "PayPal";
+            mTicketID = 10;
+
+            //always return true
+            return true;
+        }
+        
     }
 }
--- Testing4/tstPayment.cs
+++ Testing4/tstPayment.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Dynamic;
 
 namespace Testing4
 {
@@ -20,83 +21,301 @@ namespace Testing4
         public void TestPaymentID()
         {
             // Arrange
-            clsPayment payment = new clsPayment();
+            clsPayment Payment = new clsPayment();
             int expectedPaymentID = 1;
 
             // Act
-            payment.paymentID = expectedPaymentID;
+            Payment.PaymentID = expectedPaymentID;
 
             // Assert
-            Assert.AreEqual(expectedPaymentID, payment.paymentID, "Payment ID was not set correctly.");
+            Assert.AreEqual(expectedPaymentID, Payment.PaymentID, "Payment ID was not set correctly.");
         }
 
         [TestMethod]
         public void TestTransactionID()
         {
             // Arrange
-            clsPayment payment = new clsPayment();
+            clsPayment Payment = new clsPayment();
             string expectedTransactionID = "ABC-123";
 
             // Act
-            payment.transactionID = expectedTransactionID;
+            Payment.TransactionID = expectedTransactionID;
 
             // Assert
-            Assert.AreEqual(expectedTransactionID, payment.transactionID, "Transaction ID was not set correctly.");
+            Assert.AreEqual(expectedTransactionID, Payment.TransactionID, "Transaction ID was not set correctly.");
         }
 
         [TestMethod]
         public void TestAmount()
         {
-            clsPayment payment = new clsPayment();
+            clsPayment Payment = new clsPayment();
             double expectedAmount = 55.5;
 
-            payment.amount = expectedAmount;
+            Payment.Amount = expectedAmount;
 
-            Assert.AreEqual(expectedAmount, payment.amount, "Amount was not set correctly.");
+            Assert.AreEqual(expectedAmount, Payment.Amount, "Amount was not set correctly.");
         }
 
         [TestMethod]
         public void TestPaymentDate()
         {
-            clsPayment payment = new clsPayment();
+            clsPayment Payment = new clsPayment();
             DateTime expectedPaymentDate = DateTime.Now.Date;
 
-            payment.paymentDate = expectedPaymentDate;
+            Payment.PaymentDate = expectedPaymentDate;
 
-            Assert.AreEqual(expectedPaymentDate, payment.paymentDate, "Payment Date was not set correctly.");
+            Assert.AreEqual(expectedPaymentDate, Payment.PaymentDate, "Payment Date was not set correctly.");
         }
 
         [TestMethod]
         public void TestIsPaymentSuccessful()
         {
-            clsPayment payment = new clsPayment();
+            clsPayment Payment = new clsPayment();
             bool expectedIsSuccessful = true;
 
-            payment.isPaymentSuccessful = expectedIsSuccessful;
+            Payment.IsPaymentSuccessful = expectedIsSuccessful;
 
-            Assert.AreEqual(expectedIsSuccessful, payment.isPaymentSuccessful, "isPaymentSuccessful was not set correctly.");
+            Assert.AreEqual(expectedIsSuccessful, Payment.IsPaymentSuccessful, "isPaymentSuccessful was not set correctly.");
         }
 
         [TestMethod]
         public void TestPaymentMethod()
         {
-            clsPayment payment = new clsPayment();
+            clsPayment Payment = new clsPayment();
             string expectedPaymentMethod = "paypal";
 
-            payment.paymentMethod = expectedPaymentMethod;
+            Payment.PaymentMethod = expectedPaymentMethod;
 
-            Assert.AreEqual(expectedPaymentMethod, payment.paymentMethod, "Payment Method was not set correctly.");
+            Assert.AreEqual(expectedPaymentMethod, Payment.PaymentMethod, "Payment Method was not set correctly.");
         }
 
         [TestMethod]
         public void TestTicketID()
         {
-            clsPayment payment = new clsPayment();
+            clsPayment Payment = new clsPayment();
             int expectedTicketId = 45;
 
-            payment.ticketId = expectedTicketId;
+            Payment.TicketID = expectedTicketId;
 
-            Assert.AreEqual(expectedTicketId, payment.ticketId, "Ticket ID was not set correctly.");
+            Assert.AreEqual(expectedTicketId, Payment.TicketID, "Ticket ID was not set correctly.");
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            //Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            //Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            //invoke the method
+            Found = Payment.Find(PaymentID);
+
+            //test to see that the result is correct
+            Assert.IsTrue(Found);
+
+        }
+
+        [TestMethod]
+        public void TestPaymentIDFound()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            //Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            //Create a boolean to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            //invoke the method
+            Found = Payment.Find(PaymentID);
+
+            //check the payment id
+            if (Payment.PaymentID != 1)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+
+        [TestMethod]
+        public void TestTransactionIDFound()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            // Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            // Create a boolean to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            // Invoke the method
+            Found = Payment.Find(PaymentID);
+
+            // Check the TransactionID
+            if (Payment.TransactionID != "ABC-123")
+            {
+                OK = false;
+            }
+
+            // Test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestAmountFound()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            // Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            // Create a boolean to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            // Invoke the method
+            Found = Payment.Find(PaymentID);
+
+            // Check the Amount
+            if (Payment.Amount != 85.50)
+            {
+                OK = false;
+            }
+
+            // Test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPaymentDateFound()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            // Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            // Create a boolean to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            // Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            // Invoke the method
+            Found = Payment.Find(PaymentID);
+
+            // Check the PaymentDate
+            if (Payment.PaymentDate != Convert.ToDateTime("01/05/2024"))
+            {
+                OK = false;
+            }
+
+            // Test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestIsPaymentSuccessfulFound()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            // Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            // Create a boolean to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            // Invoke the method
+            Found = Payment.Find(PaymentID);
+
+            // Check the IsPaymentSuccessful
+            if (Payment.IsPaymentSuccessful != true)
+            {
+                OK = false;
+            }
+
+            // Test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPaymentMethodFound()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            // Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            // Create a boolean to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            // Invoke the method
+            Found = Payment.Find(PaymentID);
+
+            // Check the PaymentMethod
+            if (Payment.PaymentMethod != "PayPal")
+            {
+                OK = false;
+            }
+
+            // Test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestTicketIDFound()
+        {
+            // Create an instance of the class we want to create
+            clsPayment Payment = new clsPayment();
+
+            // Create a boolean variable to store the results of the search
+            Boolean Found = false;
+
+            // Create a boolean to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //Create some test data to use with the method
+            Int32 PaymentID = 1;
+
+            // Invoke the method
+            Found = Payment.Find(PaymentID);
+
+            // Check the TicketID
+            if (Payment.TicketID != 10)
+            {
+                OK = false;
+            }
+
+            // Test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
     }
 }
