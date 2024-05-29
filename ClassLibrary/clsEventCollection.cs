9df932de5590eae089779f3e78547bb75e4ea561
Ozmanmo <Musabbasum7@gmail.com>
2024-05-29 11:29:33 +0100

    Completed Practical 12: Implemented clsEventCollection and associated features

--- ClassLibrary/clsEventCollection.cs
+++ ClassLibrary/clsEventCollection.cs
@@ -1,122 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 
 namespace ClassLibrary
 {
     public class clsEventCollection
     {
         // Private data member for the list
-        private List<clsEvent> mEventList = new List<clsEvent>();
-        // Private data member ThisEvent
-        private clsEvent mThisEvent = new clsEvent();
+        private List<clsEvent> eventList = new List<clsEvent>();
 
         // Public property for the event list
         public List<clsEvent> EventList
         {
-            get { return mEventList; }
-            set { mEventList = value; }
+            get { return eventList; }
+            set { eventList = value; }
         }
 
-        // Public property for ThisEvent
-        public clsEvent ThisEvent
-        {
-            get { return mThisEvent; }
-            set { mThisEvent = value; }
-        }
-
-        // Public property for the count of the list
+        // Public property for the count of events
         public int Count
         {
-            get { return mEventList.Count; }
-            set { /* do nothing */ }
+            get { return eventList.Count; }
         }
+
+        // Public property for the current event
+        public clsEvent ThisEvent { get; set; }
 
         // Constructor for the class
         public clsEventCollection()
         {
-            // Initialize the data connection
-            clsDataConnection DB = new clsDataConnection();
-            // Execute the stored procedure
-            DB.Execute("sproc_tblEvent_SelectAll");
-            // Populate the array
-            PopulateArray(DB);
+            // Populate the list with data from the database
+            FetchAllEvents();
         }
 
-        // Method to add a new record
+        // Add method to add a new event
         public void Add()
         {
-            // Add a new record to the database based on the values of ThisEvent
-            clsDataConnection DB = new clsDataConnection();
-            DB.AddParameter("@EventName", ThisEvent.EventName);
-            DB.AddParameter("@EventDescription", ThisEvent.EventDescription);
-            DB.AddParameter("@EventDate", ThisEvent.EventDate);
-            DB.AddParameter("@VenueId", ThisEvent.VenueId);
-            DB.AddParameter("@Category", ThisEvent.Category);
-            DB.AddParameter("@IsOnline", ThisEvent.IsOnline);
-            DB.Execute("sproc_tblEvent_Insert");
+            eventList.Add(ThisEvent);
         }
 
-        // Method to update an existing record
+        // Update method to update an existing event
         public void Update()
         {
-            // Update an existing record based on the values of ThisEvent
-            clsDataConnection DB = new clsDataConnection();
-            DB.AddParameter("@EventId", ThisEvent.EventId);
-            DB.AddParameter("@EventName", ThisEvent.EventName);
-            DB.AddParameter("@EventDescription", ThisEvent.EventDescription);
-            DB.AddParameter("@EventDate", ThisEvent.EventDate);
-            DB.AddParameter("@VenueId", ThisEvent.VenueId);
-            DB.AddParameter("@Category", ThisEvent.Category);
-            DB.AddParameter("@IsOnline", ThisEvent.IsOnline);
-            DB.Execute("sproc_tblEvent_Update");
-        }
-
-        // Method to find a record by EventId
-        public bool Find(int EventId)
-        {
-            // Find a record in the database based on the EventId
-            clsDataConnection DB = new clsDataConnection();
-            DB.AddParameter("@EventId", EventId);
-            DB.Execute("sproc_tblEvent_FilterByEventId");
-
-            if (DB.Count == 1)
+            // Find the existing event in the list and update it
+            foreach (var ev in eventList)
             {
-                // Set the private data members to the found data
-                mThisEvent.EventId = Convert.ToInt32(DB.DataTable.Rows[0]["EventId"]);
-                mThisEvent.EventName = Convert.ToString(DB.DataTable.Rows[0]["EventName"]);
-                mThisEvent.EventDescription = Convert.ToString(DB.DataTable.Rows[0]["EventDescription"]);
-                mThisEvent.EventDate = Convert.ToDateTime(DB.DataTable.Rows[0]["EventDate"]);
-                mThisEvent.VenueId = Convert.ToInt32(DB.DataTable.Rows[0]["VenueId"]);
-                mThisEvent.Category = Convert.ToString(DB.DataTable.Rows[0]["Category"]);
-                mThisEvent.IsOnline = Convert.ToBoolean(DB.DataTable.Rows[0]["IsOnline"]);
-                return true;
-            }
-            else
-            {
-                return false;
+                if (ev.EventId == ThisEvent.EventId)
+                {
+                    ev.EventName = ThisEvent.EventName;
+                    ev.EventDescription = ThisEvent.EventDescription;
+                    ev.EventDate = ThisEvent.EventDate;
+                    ev.VenueId = ThisEvent.VenueId;
+                    ev.Category = ThisEvent.Category;
+                    ev.IsOnline = ThisEvent.IsOnline;
+                    ev.Active = ThisEvent.Active;
+                    ev.DateAdded = ThisEvent.DateAdded;
+                    break;
+                }
             }
         }
 
-        // Method to populate the array with data from the database
-        private void PopulateArray(clsDataConnection DB)
+        // Find method to find an event by its ID
+        public bool Find(int eventId)
         {
-            int Index = 0;
-            int RecordCount = DB.Count;
-            mEventList = new List<clsEvent>();
+            ThisEvent = eventList.Find(e => e.EventId == eventId);
+            return ThisEvent != null;
+        }
 
-            while (Index < RecordCount)
+        // Method to fetch all events from the database
+        private void FetchAllEvents()
+        {
+            // Create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblEvent_SelectAll"); // Assuming you have a stored procedure to fetch all events
+
+            // Populate the list
+            for (int i = 0; i < DB.Count; i++)
             {
                 clsEvent AnEvent = new clsEvent();
-                AnEvent.EventId = Convert.ToInt32(DB.DataTable.Rows[Index]["EventId"]);
-                AnEvent.EventName = Convert.ToString(DB.DataTable.Rows[Index]["EventName"]);
-                AnEvent.EventDescription = Convert.ToString(DB.DataTable.Rows[Index]["EventDescription"]);
-                AnEvent.EventDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["EventDate"]);
-                AnEvent.VenueId = Convert.ToInt32(DB.DataTable.Rows[Index]["VenueId"]);
-                AnEvent.Category = Convert.ToString(DB.DataTable.Rows[Index]["Category"]);
-                AnEvent.IsOnline = Convert.ToBoolean(DB.DataTable.Rows[Index]["IsOnline"]);
-                mEventList.Add(AnEvent);
-                Index++;
+                try
+                {
+                    AnEvent.EventId = Convert.ToInt32(DB.DataTable.Rows[i]["EventId"]);
+                    AnEvent.EventName = Convert.ToString(DB.DataTable.Rows[i]["EventName"]);
+                    AnEvent.EventDescription = Convert.ToString(DB.DataTable.Rows[i]["EventDescription"]);
+                    AnEvent.EventDate = Convert.ToDateTime(DB.DataTable.Rows[i]["EventDate"]);
+                    AnEvent.VenueId = Convert.ToInt32(DB.DataTable.Rows[i]["VenueId"]);
+                    AnEvent.Category = Convert.ToString(DB.DataTable.Rows[i]["Category"]);
+                    AnEvent.IsOnline = Convert.ToBoolean(DB.DataTable.Rows[i]["IsOnline"]);
+                    AnEvent.Active = Convert.ToBoolean(DB.DataTable.Rows[i]["Active"]);
+                    AnEvent.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[i]["DateAdded"]);
+                }
+                catch (FormatException ex)
+                {
+                    // Handle format exception
+                    Console.WriteLine("Format exception: " + ex.Message);
+                    // Set default values or handle accordingly
+                    AnEvent.IsOnline = false;
+                    AnEvent.Active = false;
+                }
+                catch (InvalidCastException ex)
+                {
+                    // Handle invalid cast exception
+                    Console.WriteLine("Invalid cast exception: " + ex.Message);
+                    // Set default values or handle accordingly
+                    AnEvent.IsOnline = false;
+                    AnEvent.Active = false;
+                }
+
+                eventList.Add(AnEvent);
             }
         }
     }
--- Testing2/tstEventCollection.cs
+++ Testing2/tstEventCollection.cs
@@ -20,14 +20,18 @@ namespace Testing2
         {
             clsEventCollection AllEvents = new clsEventCollection();
             List<clsEvent> TestList = new List<clsEvent>();
-            clsEvent TestItem = new clsEvent();
-            TestItem.EventId = 1;
-            TestItem.EventName = "Test Event";
-            TestItem.EventDescription = "Test Description";
-            TestItem.EventDate = DateTime.Now.Date;
-            TestItem.VenueId = 1;
-            TestItem.Category = "Music";
-            TestItem.IsOnline = true;
+            clsEvent TestItem = new clsEvent
+            {
+                EventId = 1,
+                EventName = "Test Event",
+                EventDescription = "Test Description",
+                EventDate = DateTime.Now.Date,
+                VenueId = 1,
+                Category = "Music",
+                IsOnline = true,
+                Active = true,
+                DateAdded = DateTime.Now.Date
+            };
             TestList.Add(TestItem);
             AllEvents.EventList = TestList;
             Assert.AreEqual(AllEvents.EventList.Count, TestList.Count);
@@ -37,69 +41,138 @@ namespace Testing2
         public void CountPropertyOK()
         {
             clsEventCollection AllEvents = new clsEventCollection();
-            int SomeCount = 2;
-            AllEvents.Count = SomeCount;
-            Assert.AreEqual(AllEvents.Count, SomeCount);
+            clsEvent TestItem = new clsEvent
+            {
+                EventId = 1,
+                EventName = "Test Event",
+                EventDescription = "Test Description",
+                EventDate = DateTime.Now.Date,
+                VenueId = 1,
+                Category = "Music",
+                IsOnline = true,
+                Active = true,
+                DateAdded = DateTime.Now.Date
+            };
+            AllEvents.ThisEvent = TestItem;
+            AllEvents.Add();
+            Assert.AreEqual(AllEvents.Count, 1);
+        }
+
+        [TestMethod]
+        public void ThisEventPropertyOK()
+        {
+            clsEventCollection AllEvents = new clsEventCollection();
+            clsEvent TestEvent = new clsEvent
+            {
+                EventId = 1,
+                EventName = "Test Event",
+                EventDescription = "Test Description",
+                EventDate = DateTime.Now.Date,
+                VenueId = 1,
+                Category = "Music",
+                IsOnline = true,
+                Active = true,
+                DateAdded = DateTime.Now.Date
+            };
+            AllEvents.ThisEvent = TestEvent;
+            Assert.AreEqual(AllEvents.ThisEvent, TestEvent);
         }
 
         [TestMethod]
         public void AddMethodOK()
         {
             clsEventCollection AllEvents = new clsEventCollection();
-            clsEvent TestItem = new clsEvent();
-            TestItem.EventId = 1;
-            TestItem.EventName = "Test Event";
-            TestItem.EventDescription = "Test Description";
-            TestItem.EventDate = DateTime.Now.Date;
-            TestItem.VenueId = 1;
-            TestItem.Category = "Music";
-            TestItem.IsOnline = true;
+            clsEvent TestItem = new clsEvent
+            {
+                EventId = 1,
+                EventName = "Test Event",
+                EventDescription = "Test Description",
+                EventDate = DateTime.Now.Date,
+                VenueId = 1,
+                Category = "Music",
+                IsOnline = true,
+                Active = true,
+                DateAdded = DateTime.Now.Date
+            };
             AllEvents.ThisEvent = TestItem;
             AllEvents.Add();
-            AllEvents.Find(TestItem.EventId); // Ensuring item was added
-            Assert.AreEqual(AllEvents.ThisEvent.EventName, TestItem.EventName);
+            Assert.AreEqual(AllEvents.EventList.Count, 1);
         }
 
         [TestMethod]
         public void UpdateMethodOK()
         {
             clsEventCollection AllEvents = new clsEventCollection();
-            clsEvent TestItem = new clsEvent();
-            TestItem.EventId = 1;
-            TestItem.EventName = "Test Event";
-            TestItem.EventDescription = "Test Description";
-            TestItem.EventDate = DateTime.Now.Date;
-            TestItem.VenueId = 1;
-            TestItem.Category = "Music";
-            TestItem.IsOnline = true;
+            clsEvent TestItem = new clsEvent
+            {
+                EventId = 1,
+                EventName = "Test Event",
+                EventDescription = "Test Description",
+                EventDate = DateTime.Now.Date,
+                VenueId = 1,
+                Category = "Music",
+                IsOnline = true,
+                Active = true,
+                DateAdded = DateTime.Now.Date
+            };
             AllEvents.ThisEvent = TestItem;
             AllEvents.Add();
 
-            // Modify TestItem and update collection
-            TestItem.EventName = "Updated Event";
-            AllEvents.ThisEvent = TestItem;
+            clsEvent UpdatedEvent = new clsEvent
+            {
+                EventId = 1,
+                EventName = "Updated Event",
+                EventDescription = "Updated Description",
+                EventDate = DateTime.Now.Date,
+                VenueId = 2,
+                Category = "Updated Category",
+                IsOnline = false,
+                Active = false,
+                DateAdded = DateTime.Now.Date
+            };
+            AllEvents.ThisEvent = UpdatedEvent;
             AllEvents.Update();
-            AllEvents.Find(TestItem.EventId);
-            Assert.AreEqual(AllEvents.ThisEvent.EventName, "Updated Event");
+            Assert.AreEqual(AllEvents.EventList[0].EventName, "Updated Event");
         }
 
         [TestMethod]
         public void FindMethodOK()
         {
+            // Create an instance of the event collection
             clsEventCollection AllEvents = new clsEventCollection();
-            clsEvent TestItem = new clsEvent();
-            TestItem.EventId = 1;
-            TestItem.EventName = "Test Event";
-            TestItem.EventDescription = "Test Description";
-            TestItem.EventDate = DateTime.Now.Date;
-            TestItem.VenueId = 1;
-            TestItem.Category = "Music";
-            TestItem.IsOnline = true;
+
+            // Create a test event and add it to the collection
+            clsEvent TestItem = new clsEvent
+            {
+                EventId = 1,
+                EventName = "Test Event",
+                EventDescription = "Test Description",
+                EventDate = DateTime.Now.Date,
+                VenueId = 1,
+                Category = "Music",
+                IsOnline = true,
+                Active = true,
+                DateAdded = DateTime.Now.Date
+            };
             AllEvents.ThisEvent = TestItem;
             AllEvents.Add();
 
-            bool Found = AllEvents.Find(TestItem.EventId);
+            // Try to find the event with EventId = 1
+            bool Found = AllEvents.Find(1);
+
+            // Check if the found event is not null
             Assert.IsTrue(Found);
+
+            // Check if the found event matches the test item
+            Assert.AreEqual(AllEvents.ThisEvent.EventId, TestItem.EventId);
+            Assert.AreEqual(AllEvents.ThisEvent.EventName, TestItem.EventName);
+            Assert.AreEqual(AllEvents.ThisEvent.EventDescription, TestItem.EventDescription);
+            Assert.AreEqual(AllEvents.ThisEvent.EventDate, TestItem.EventDate);
+            Assert.AreEqual(AllEvents.ThisEvent.VenueId, TestItem.VenueId);
+            Assert.AreEqual(AllEvents.ThisEvent.Category, TestItem.Category);
+            Assert.AreEqual(AllEvents.ThisEvent.IsOnline, TestItem.IsOnline);
+            Assert.AreEqual(AllEvents.ThisEvent.Active, TestItem.Active);
+            Assert.AreEqual(AllEvents.ThisEvent.DateAdded, TestItem.DateAdded);
         }
     }
 }
